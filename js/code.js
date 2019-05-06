d9910c0ed9d5ee9ff0a170b0a1cb399699b9da52
Yevhenii <yevhenii.shut@diya.pro>
2019-05-06 19:53:08 +0300

    Это наш первый комит

---
+++ js/code.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/*var a = 5;
+console.log(a);
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jgp','apple.bmp'];
+console.log(arr[2]);
+
+alert("Hello world!");
+
+let answer = confirm("Are you ok?");
+
+console.log(answer);
+
+let answer = +prompt ("are you 18?", "da")
+console.log(typeof(answer));
+
+console.log("arr" + " - object");
+console.log(4 + + " - object");
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClosed = false;
+
+console.log(isChecked || !isClosed); */
